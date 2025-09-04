dd6d12d388e967ae2492c7d055416fe697b37d09
Charles <Charlessammarco@gmail.com>
2025-09-04 13:11:40 -0500

    added starters menu

--- nell-s/src/pages/Menu.tsx
+++ nell-s/src/pages/Menu.tsx
@@ -1,7 +1,7 @@
 function Menu() {
   return (
     <div>
-      <h1>Menu page</h1>
+      <h1>Dine-In Menu</h1>
       <div>
         <h2>Starters & Sharables</h2>
         <div>
@@ -10,7 +10,7 @@ function Menu() {
             Slow-cooked in our special marinade and topped with Grandpa’s smokey
             sauce
           </p>
-          <span>Half $12.95 | Full $21.95</span>
+          <h4>Half $12.95 | Full $21.95</h4>
         </div>
         <div>
           <h3>Fried Calamari</h3>
@@ -18,7 +18,106 @@ function Menu() {
             Marinated and lightly breaded and fried. Served with tangy cocktail
             sauce and lemon
           </p>
-          <span>$15.95</span>
+          <h4>$15.95</h4>
+        </div>
+                <div>
+          <h3>Stuffed Mushroom Caps</h3>
+          <p>
+            Mozzarella cheese, herbs, bread crumbs, and olive oil are tossed and baked for this crowd-pleaser
+          </p>
+          <h4>$9.95</h4>
+        </div>
+                        <div>
+          <h3>Cheesy Potato Skins</h3>
+          <p>
+Stuffed with bacon, cheddar, and mozzarella cheese with sour cream on the side          </p>
+          <h4>$15.95</h4>
+        </div>                <div>
+          <h3>Chicken Tenders</h3>
+          <p>
+A classic snack with BBQ sauce for dipping.         </p>
+          <h4>$10.95</h4>
+        </div>                <div>
+          <h3>Jumbo Shrimp Cocktail</h3>
+          <p>
+A classic with lemon and cocktail sauce          </p>
+          <h4>$15.95</h4>
+        </div>                <div>
+          <h3>Chicken Tenders</h3>
+          <p>
+A classic snack with BBQ sauce for dipping.          </p>
+          <h4>$10.95</h4>
+          <h5>Buffalo Style +$1.00 w/ peppercorn ranch</h5>
+        </div>                <div>
+          <h3>Grilled Shrimp Skewers</h3>
+          <p>
+6 shrimp with a dash of our special spices, lightly grilled – served w/ remoulade sauce, lemon, & rye bread garnish          </p>
+          <h4>$15.95</h4>
+        </div>                <div>
+          <h3>Chilean Blue Mussels</h3>
+          <p>
+Veggies & Breadstick in a wonderful wine broth          </p>
+          <h4>$16.95</h4>
+        </div>                <div>
+          <h3>Sausage & Peppers</h3>
+
+          <h4> $9.95</h4>
+        </div>                <div>
+          <h3>Basket of Fries</h3>
+          <h4>$6.25</h4>
+        </div>                <div>
+          <h3>Burratta, Bruschetta & Olive Board</h3>
+          <p>
+With toasted garlic bread crisps          </p>
+          <h4>$13.95</h4>
+        </div>                <div>
+          <h3>Lump Crab Cakes</h3>
+          <p>
+Pan-seared and served with chipotle sauce and lemon          </p>
+          <h4>$15.95</h4>
+        </div>                <div>
+          <h3>Basket of Sweeet Potato Fries</h3>
+          <h4>$7.95</h4>
+        </div>                <div>
+          <h3>Mozzarella Stix</h3>
+          <p>
+Lightly coated in breadcrumbs served with crushed tomato basil marinara          </p>
+          <h4>$8.95</h4>
+        </div>                <div>
+          <h3>Buffalo Hot Wings</h3>
+          <p>
+            Tossed in our buffalo sauce and served with celery sticks and bleu cheese sauce
+          </p>
+          <h4>6pc $9.95 | 12pc $17.95 | 20pc $26.95</h4>
+        </div>                <div>
+          <h3>Bruschetta</h3>
+          <p>
+            Mozzarella cheese, herbs, bread crumbs, and olive oil are tossed and baked for this crowd-pleaser
+          </p>
+          <h4>$8.75</h4>
+        </div>                <div>
+          <h3>Cauliflower Buffalo Bites</h3>
+          <p>
+Fresh cauliflower bites tossed. in a tangy buffalo sauce served with our homemade Peppercorn Ranch. Also available plain, with our sweet chili glaze          </p>
+          <h4>$8.95</h4>
+        </div>
+        <div>
+          <h3>The Greatest</h3>
+          <p>
+The name says it all! An assortment of ribs, fried shrimp, stuffed mushroom caps, chicken tenders, onion rings, and fresh vegetables with ranch, BBQ, and cocktail sauces for dipping          </p>
+          <h4>$22.95</h4>
+        </div>
+        <div>
+          <h3>Hot Giant Pretzel </h3>
+          <p>
+Savory treat served with hot cheesy dipping sauce          </p>
+          <h4>$10.95</h4>
+        </div>
+        <div>
+          <h3>Pot Stickers</h3>
+          <p>
+Pork and vegetable dumplings steamed and seared, served with sweet chili glaze          </p>
+          <h4>$10.95</h4>
         </div>
       </div>
     </div>
