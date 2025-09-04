4a6039c32df2b2834e9d3463aadc19a1b9088177
Charles <Charlessammarco@gmail.com>
2025-09-04 12:10:31 -0500

    added menu items start

--- nell-s/src/App.tsx
+++ nell-s/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./Navbar.tsx";
 import { Outlet } from "react-router-dom";
 import NellsLogo from "./assets/Nells.svg";
-import blurredbackground from "./assets/blurredbackground.jpg";
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
           <Navbar />
           <div className="page-content">
             <Outlet />
-            {/* <img src={blurredbackground}></img> */}
+    
           </div>
         </div>
       </header>
--- nell-s/src/pages/Menu.tsx
+++ nell-s/src/pages/Menu.tsx
@@ -1,9 +1,27 @@
-function Menu(){
-    return(
+function Menu() {
+  return (
+    <div>
+      <h1>Menu page</h1>
+      <div>
+        <h2>Starters & Sharables</h2>
         <div>
-            <p>Menu page</p>
+          <h3>Grandpa's Baby Back Ribs</h3>
+          <p>
+            Slow-cooked in our special marinade and topped with Grandpa’s smokey
+            sauce
+          </p>
+          <span>Half $12.95 | Full $21.95</span>
         </div>
-    )
-
+        <div>
+          <h3>Fried Calamari</h3>
+          <p>
+            Marinated and lightly breaded and fried. Served with tangy cocktail
+            sauce and lemon
+          </p>
+          <span>$15.95</span>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Menu
+export default Menu;
