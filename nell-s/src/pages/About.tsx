5a603718019017d434ad1a6d8dd0af92c24b3aaf
Charles <Charlessammarco@gmail.com>
2025-10-29 14:17:41 -0500

    added about

--- nell-s/src/pages/About.tsx
+++ nell-s/src/pages/About.tsx
@@ -1,9 +1,54 @@
-function About(){
-    return(
-        <div>
-            <p>About page</p>
-        </div>
-    )
-
+function About() {
+  return (
+    <div>
+      <h1>About Nell's</h1>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Molestias id voluptatibus porro amet aliquid dolorum
+        nostrum reprehenderit tempore accusantium. Alias impedit molestiae
+        sapiente quasi rerum dignissimos veniam minima doloremque eaque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque?
+      </h2>
+      <img src="/src/assets/bbq.jpg" alt="bbq on grill"></img>
+      <img src="/src/assets/friends1.jpg" alt="criends cheering"></img>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Molestias id voluptatibus porro amet aliquid dolorum
+        nostrum reprehenderit tempore accusantium. Alias impedit molestiae
+        sapiente quasi rerum dignissimos veniam minima doloremque eaque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque?
+      </h2>
+      <img src="/src/assets/ribs.jpg" alt="rib platter"></img>
+      <img src="/src/assets/date1.jpg" alt="date night"></img>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Molestias id voluptatibus porro amet aliquid dolorum
+        nostrum reprehenderit tempore accusantium. Alias impedit molestiae
+        sapiente quasi rerum dignissimos veniam minima doloremque eaque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Molestias id
+        voluptatibus porro amet aliquid dolorum nostrum reprehenderit tempore
+        accusantium. Alias impedit molestiae sapiente quasi rerum dignissimos
+        veniam minima doloremque eaque?
+      </h2>
+      <img src="/src/assets/steak.jpg" alt="steak plate"></img>
+      <img src="/src/assets/burgers.jpg" alt="bbq on grill"></img>
+      <img src="/src/assets/friends3.jpg" alt="bbq on grill"></img>
+    </div>
+  );
 }
-export default About
+export default About;
