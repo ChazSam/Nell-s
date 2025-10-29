b339249fa564121d4bf5f6f55512bc16ee2f5966
Charles <Charlessammarco@gmail.com>
2025-10-29 13:44:26 -0500

    added reviews

--- nell-s/src/pages/Reviews.tsx
+++ nell-s/src/pages/Reviews.tsx
@@ -1,7 +1,32 @@
 function Reviews(){
     return(
         <div>
-            <p>Reviews page</p>
+            <h1>Customer Reviews</h1>
+            <div>
+                <p>Dave G.</p>
+                <p>⭐⭐⭐⭐⭐</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
+            </div>
+            <div>
+                <p>Joe B.</p>
+                <p>⭐⭐⭐ 1/2</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
+            </div>
+            <div>
+                <p>Stephanie S.</p>
+                <p>⭐⭐⭐⭐⭐</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
+            </div>
+            <div>
+                <p>Jen K.</p>
+                <p>⭐⭐⭐⭐</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
+            </div>
+            <div>
+                <p>Charlie Z.</p>
+                <p>⭐⭐⭐⭐ 1/2</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
+            </div>
         </div>
     )
 
