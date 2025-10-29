67d993ac21320b98261e4a10c39466b2039e6755
Charles <Charlessammarco@gmail.com>
2025-10-29 15:38:02 -0500

    added home page

--- nell-s/src/pages/Home.tsx
+++ nell-s/src/pages/Home.tsx
@@ -1,7 +1,13 @@
 function Home(){
     return(
         <div>
-            <p>Home page</p>
+            <h1>Welcome To Nell's Bar And Grill</h1>
+            <img src="/src/assets/friends2.jpg"></img>
+            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit deserunt dolor alias quasi provident laborum et minima neque doloribus. Iusto voluptas sint perferendis, facere molestias quidem aspernatur perspiciatis? Accusamus, nisi?</h2>
+            <img src="/src/assets/ChickenWings.jpg"></img>
+                        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit deserunt dolor alias quasi provident laborum et minima neque doloribus. Iusto voluptas sint perferendis, facere molestias quidem aspernatur perspiciatis? Accusamus, nisi?</h2>
+            <img src="/src/assets/bbq.jpg"></img>
+                        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit deserunt dolor alias quasi provident laborum et minima neque doloribus. Iusto voluptas sint perferendis, facere molestias quidem aspernatur perspiciatis? Accusamus, nisi?</h2>
         </div>
     )
 
