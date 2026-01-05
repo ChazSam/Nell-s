0aa5bcf9a7254adeaae36444106fc2c9929ae880
Charles <Charlessammarco@gmail.com>
2026-01-05 17:25:46 -0600

    updated fonts

--- nell-s/src/pages/Reviews.tsx
+++ nell-s/src/pages/Reviews.tsx
@@ -2,28 +2,28 @@ function Reviews(){
     return(
         <div className="review-page">
             <h1>Customer Reviews</h1>
-            <div>
-                <p>Dave G.</p>
+            <div className="review-items">
+                <h3>Dave G.</h3>
                 <p>⭐⭐⭐⭐⭐</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
             </div>
-            <div>
-                <p>Joe B.</p>
+            <div  className="review-items">
+                <h3>Joe B.</h3>
                 <p>⭐⭐⭐ 1/2</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
             </div>
-            <div>
-                <p>Stephanie S.</p>
+            <div  className="review-items">
+                <h3>Stephanie S.</h3>
                 <p>⭐⭐⭐⭐⭐</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
             </div>
-            <div>
-                <p>Jen K.</p>
+            <div  className="review-items">
+                <h3>Jen K.</h3>
                 <p>⭐⭐⭐⭐</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
             </div>
-            <div>
-                <p>Charlie Z.</p>
+            <div className="review-items">
+                <h3>Charlie Z.</h3>
                 <p>⭐⭐⭐⭐ 1/2</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, natus architecto? Illo cupiditate atque dolor tempore? Commodi odio nostrum ea magnam quas veritatis, dolorem odit accusantium pariatur? Quod, corporis porro?</p>
             </div>
