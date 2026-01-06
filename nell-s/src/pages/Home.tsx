4acf262357d7aa87f30bccf8c06d8f2960a3ac49
Charles <Charlessammarco@gmail.com>
2026-01-05 18:12:06 -0600

    carousel start

--- nell-s/src/pages/Home.tsx
+++ nell-s/src/pages/Home.tsx
@@ -2,6 +2,21 @@ function Home() {
   return (
     <div className="home-page">
       <h1>Welcome To Nell's Bar And Grill</h1>
+
+      <div className="gallery">
+      <ul>
+        <li>
+          <img src="/src/assets/friends1.jpg" alt="" />
+        </li>
+        <li>
+          <img src="/src/assets/date1.jpg" alt="" />
+        </li>
+        <li>
+          <img src="/src/assets/friends3.jpg" alt="" />
+        </li>
+
+      </ul>
+      </div>
       <img src="/src/assets/friends2.jpg"></img>
       <h3>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
