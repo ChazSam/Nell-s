36131584623a5317c9b47195d9c3dfac4fc2266c
Charles <Charlessammarco@gmail.com>
2026-02-03 17:36:48 -0600

    added navbar visiblity on hamburger click

--- nell-s/src/App.tsx
+++ nell-s/src/App.tsx
@@ -2,26 +2,24 @@ import "./App.css";
 import Navbar from "./Navbar.tsx";
 import { Outlet } from "react-router-dom";
 import NellsLogo from "./assets/Nells.svg";
-import {useState} from 'react';
+import { useState } from "react";
 
 function App() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <header>
         <img src={NellsLogo} alt="Nell's Logo" className="nells-logo"></img>
         <div>
-
-      <div className="hamburger" onClick={()=> setIsOpen(!isOpen)}>
-        <div className="burger"></div>
-        <div className="burger"></div>
-        <div className="burger"></div>
-      </div>
-
-          <Navbar />
+          <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+            <div className="burger"></div>
+            <div className="burger"></div>
+            <div className="burger"></div>
+          </div>
+          
+          {isOpen && <Navbar />}
           <div className="page-content">
             <Outlet />
-    
           </div>
         </div>
       </header>
--- nell-s/src/Navbar.tsx
+++ nell-s/src/Navbar.tsx
@@ -1,57 +1,49 @@
 import { NavLink } from "react-router-dom";
 
-
-
 function Navbar() {
-
-
-
   return (
     <>
-    <nav className="navbar">
+      <nav className="navbar">
+        <NavLink
+          to="/"
+          className={({ isActive }: { isActive: boolean }) =>
+            isActive ? "active-link" : ""
+          }
+          end
+        >
+          Home
+        </NavLink>
 
- 
+        <NavLink
+          to="/about"
+          className={({ isActive }: { isActive: boolean }) =>
+            isActive ? "active-link" : ""
+          }
+          end
+        >
+          About
+        </NavLink>
 
-      <NavLink
-        to="/"
-        className={({ isActive }: { isActive: boolean }) =>
-          isActive ? "active-link" : ""
-      }
-      end
-      >
-        Home
-      </NavLink>
+        <NavLink
+          to="/menu"
+          className={({ isActive }: { isActive: boolean }) =>
+            isActive ? "active-link" : ""
+          }
+          end
+        >
+          Menu
+        </NavLink>
 
-      <NavLink
-        to="/about"
-        className={({ isActive }: { isActive: boolean }) =>
-          isActive ? "active-link" : ""
-      }
-      end
-      >
-        About
-      </NavLink>
-
-      <NavLink
-        to="/menu"
-        className={({ isActive }: { isActive: boolean }) =>
-          isActive ? "active-link" : ""
-      }
-      end
-      >
-        Menu
-      </NavLink>
-
-      <NavLink
-        to="/reviews"
-        className={({ isActive }: { isActive: boolean }) =>
-          isActive ? "active-link" : ""
-      }
-      end
-      >
-        Reviews
-      </NavLink>
-    </nav>
+        <NavLink
+          to="/reviews"
+          className={({ isActive }: { isActive: boolean }) =>
+            isActive ? "active-link" : ""
+          }
+          end
+        >
+          Reviews
+        </NavLink>
+      </nav>
     </>
   );
 }
